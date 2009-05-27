6b83a31820c8dc8bdae1aef60fcdfb2da8d558d7
onotelli <onotelli@1140968e-e2c7-4c3d-a3be-3139d5afca7e>
2009-05-27 13:36:01 +0000

    git-svn-id: svn://svn.code.sf.net/p/justniffer/code/trunk@82 1140968e-e2c7-4c3d-a3be-3139d5afca7e

---
+++ python/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+#	Copyright (c) 2009 Plecno s.r.l. All Rights Reserved 
+#	info@plecno.com
+#	via Giovio 8, 20144 Milano, Italy
+#	Released under the terms of the GPLv3 or later
+#	Author: Oreste Notelli <oreste.notelli@plecno.com>	
+
+import commands
+import shutil
+import os
+
+
+e = commands.getstatusoutput
+s, p = e ("which justniffer")
+if (s == 0):
+  scripts_path = p.replace("bin/justniffer", "share/justniffer/scripts")
+  os.makedirs(scripts_path)
+  e ("chmod a+x http_parser.py") 
+  shutil.copy("common.py",scripts_path)
+  shutil.copy("http_parser.py",scripts_path)
+  bin_path = p.replace("bin/justniffer", "bin")
+  e ("chmod a+x create_scripts") 
+  e ("./create_scripts justniffer-grab-http-traffic.in justniffer-grab-http-traffic " + scripts_path)
+  e ("chmod a+x justniffer-grab-http-traffic") 
+  shutil.copy("justniffer-grab-http-traffic", bin_path)
+
+  print "INSTALLAZIONE COMPLETATA"
+
