a77c81b95a0bf67beed8be63795c98461e85d658
onotelli <onotelli@1140968e-e2c7-4c3d-a3be-3139d5afca7e>
2009-07-26 12:50:15 +0000

    git-svn-id: svn://svn.code.sf.net/p/justniffer/code/trunk@135 1140968e-e2c7-4c3d-a3be-3139d5afca7e

---
+++ make-release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+JUST_VERSION=0.5.6
+
+TMP_DIR=/tmp
+JUST_DIR=justniffer
+JUST_DIR_VER=$JUST_DIR-$JUST_VERSION
+
+JUST_TMP_DIR=$TMP_DIR/$JUST_DIR
+
+rm -Rf $JUST_TMP_DIR
+cd $TMP_DIR
+mkdir $JUST_DIR
+cd $JUST_DIR
+svn co https://justniffer.svn.sourceforge.net/svnroot/justniffer/trunk/ $JUST_DIR_VER
+cd $JUST_DIR_VER
+find ./ -name .svn -exec rm -Rf {} \;
+find ./ -name .svn -exec rm  {} \;
+dpkg-buildpackage -d
+
