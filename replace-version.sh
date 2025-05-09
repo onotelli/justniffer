7c35d41adaf5999284175b9c3e42de1bd53bb8c0
Oreste Notelli <oreste.notelli@gmail.com>
2025-05-09 22:04:29 +0200

    debian build docker

--- replace-version.sh
+++ replace-version.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 TARGET_VERSION=$DEBIAN_VERSION
-cd /workspace/justniffer
-version=$(/workspace/justniffer/src/justniffer --version | awk 'NR==1{print $2}')
+# exit if version not specified
+if [ -z "$TARGET_VERSION" ]; then
+    echo "No version specified"
+    exit 1 
+fi
+cd /workspace
+version=$(/workspace/src/justniffer --version | awk 'NR==1{print $2}')
 escaped_version=$(sed 's/\./\\./g' <<< "$version")
 sed -i "1s/(${escaped_version}\.[^)]*) [^;]*;/(${version}.${TARGET_VERSION}) ${TARGET_VERSION};/" debian/changelog
