444dc96c7c5dcef79c5a36c159d8d628bd2b83c1
Oreste Notelli <oreste.notelli@gmail.com>
2025-05-10 14:49:55 +0200

    build scripts updated

---
+++ main/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dpkg-buildpackage -S -sa
--- main/replace-version.sh
+++ main/replace-version.sh
@@ -6,7 +6,7 @@ if [ -z "$TARGET_VERSION" ]; then
     echo "No version specified"
     exit 1 
 fi
-cd /workspace
-version=$(/workspace/src/justniffer --version | awk 'NR==1{print $2}')
+BASE_DIR=.
+version=$(echo "AC_INIT([justniffer],[0.6.7],[info@plecno.com])" | awk -F'[][]' '{print $4}')
 escaped_version=$(sed 's/\./\\./g' <<< "$version")
 sed -i "1s/(${escaped_version}\.[^)]*) [^;]*;/(${version}.${TARGET_VERSION}) ${TARGET_VERSION};/" debian/changelog
