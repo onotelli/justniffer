2c2e38e68fe3c80e29ac1cb5961a4670d735fdf6
Oreste Notelli <oreste.notelli@gmail.com>
2026-03-29 19:16:48 +0200

    justniffer-cli removed

--- python/justniffer-cli/justniffer/logging.py
+++
@@ -1,7 +0,0 @@
-from os import environ
-from justniffer.settings import settings
-ENV_VARIABLE = 'LOGURU_LEVEL'
-
-log_level = settings.log_level
-environ[ENV_VARIABLE] = log_level
-from loguru import logger
